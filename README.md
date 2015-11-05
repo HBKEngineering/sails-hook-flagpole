1b8eb505a092027791e91361801f2e41cdb7aef8
Brett Neese <brett@brettneese.com>
2015-11-04 22:34:46 -0600

    almost working

---
+++ README.md
@@ -0,0 +1,45 @@
+<p align="center"><img src="/../images/sails.png?raw=true" height="80"/><img src="/../images/jsonapi.png?raw=true" height="80"/></p>
+
+# sails-hook-flagpole
+
+This is a [Sails JS](http://sailsjs.org) hook for creating an API which conforms to JSON API specifications per jsonapi.org.
+
+*Note*: This project is still in development, and is not yet fully functional. 
+
+### Installation
+
+When we get on NPM:
+
+`npm install sails-hook-flagpole`
+
+### Usage
+*requires at least sails >= 0.11*
+
+Just lift your app as normal, and your api responses will be formatted in accordance with [jsonapi.org](http://jsonapi.org/format/).
+
+#### Options
+Create a `jsonapi.js` file inside the `config/` directory of your app, and you can set the following options:
+
+| Option        | Default   |  Description  |
+|---------------|:---------:|---------------|
+| `compoundDoc` |  `true`   | When set to 'true' (default), response will be a [compound document](http://jsonapi.org/format/#document-compound-documents). Otherwise, related resources will be nested within the top-level resource attributes. |
+| `included`    |  `true`   | When set to `true` (default), related resource data will be [included](http://jsonapi.org/format/#fetching-includes) in the response. Currently, `include` parameters in client requests are not supported.  |
+
+
+### Known Limitations
+
+This is still a project-in-work.  So far, the following are not yet implemented:
+
+- [ ] Included request parameter handling (400 response if present)
+- [ ] Links
+  - [ ] Top-level "self" links
+  - [ ] Top-level "related" links
+  - [ ] Resource-level "self" links
+  - [ ] Related resource relationship links
+  - [ ] Metadata links
+- [ ] Pagination
+- [ ] Formatting
+  - [ ] Non-dasherized attributes
+- [ ] Sparse fieldsets
+
+There may be more.  Please submit issue reports.
---
+++ tests/responses/ok-test.js
@@ -0,0 +1,211 @@
+/* global User, Author, Book */
+'use strict';
+
+let Barrels = require('barrels');
+let path = require('path');
+let Sails = require('sails').Sails;
+let test = require('tape-catch');
+let loadConfig = require('../helpers/load-config');
+let fixtures,
+    sails;
+
+test('Sails does not crash', function (t) {
+  // Attempt to lift sails
+  Sails().load(loadConfig, function (err, _sails) {
+    if (err) {
+      t.fail(err);
+    }
+    sails = _sails;
+    let barrels = new Barrels(path.join(process.cwd(), '/tests/fixtures'));
+    fixtures = barrels.data;
+    barrels.populate(function (err) {
+      if (err) {
+        t.fail(err);
+      }
+      t.end(err);
+    });
+  });
+});
+
+test('User model and fixture is loaded correctly', function (t) {
+  t.plan(3);
+
+  User.find().exec(function (err, users) {
+    if (err) {
+      t.fail(err);
+    }
+    t.ok(users, 'User model exists');
+    t.ok(fixtures['user'], 'user fixture loaded');
+    if (users && fixtures['user']) {
+      t.equal(fixtures['user'].length, users.length, 'User model contains the same number of items as fixture');
+    } else {
+      t.end();
+    }
+  });
+});
+
+test('Author model and fixture is loaded correctly', function (t) {
+  t.plan(3);
+
+  Author.find().exec(function (err, authors) {
+    if (err) {
+      t.fail(err);
+    }
+    t.ok(authors, 'Author model exists');
+    t.ok(fixtures['author'], 'author fixture loaded');
+    if (authors && fixtures['author']) {
+      t.equal(fixtures['author'].length, authors.length, 'Author model contains the same number of items as fixture');
+    } else {
+      t.end();
+    }
+  });
+});
+
+test('Book model and fixture is loaded correctly', function (t) {
+  t.plan(3);
+
+  Book.find().exec(function (err, books) {
+    if (err) {
+      t.fail(err);
+    }
+    t.ok(books, 'Book model exists');
+    t.ok(fixtures['book'], 'book fixture loaded');
+    if (books && fixtures['book']) {
+      t.equal(fixtures['book'].length, books.length, 'Book model contains the same number of items as fixture');
+    } else {
+      t.end();
+    }
+  });
+});
+
+test('Returns correct attributes', function (t) {
+  t.plan(7);
+
+  sails.request({
+    url   : '/user',
+    method: 'GET',
+  }, function (err, res, body) {
+    if (err) {
+      t.fail(err);
+    }
+    try {
+      t.ok(body.data, 'Body contains a "data" property');
+      t.equal(body.data.length, 2, 'There are two user objects');
+      t.equal(body.data[0].id, '1', 'First model id is 1');
+      t.equal(body.data[0].type, 'users', 'First model type is "users"');
+      t.ok(body.data[0].attributes, '"data" contains an "attributes" property');
+      t.ok(body.data[0].attributes['first-name'], '"attributes" contains a "first-name" property');
+      t.equal(body.data[0].attributes['first-name'], 'Peter', '"first-name" of the first model is "Peter"');
+    } catch (err) {
+      t.fail(err);
+    }
+    t.end();
+  });
+
+});
+
+test('Returns correct nested resources', function (t) {
+  t.plan(3);
+
+  sails.config.flagpole.compoundDoc = false;
+
+  sails.request({
+    url   : '/author',
+    method: 'GET',
+  }, function (err, res, body) {
+    if (err) {
+      t.fail(err);
+    }
+    try {
+      t.ok(body.data[0].attributes.books, '"attributes" contains a "books" property');
+      t.equal(typeof body.data[0].attributes.books, 'object', '"books" is an object');
+      t.deepEqual(body.data[0].attributes.books[0].title, 'A Game of Thrones', '"title" of first book is "A Game of Thrones"');
+    } catch (err) {
+      t.fail(err);
+    }
+    t.end();
+  });
+});
+
+test('Returns relationships for compound document', function (t) {
+  t.plan(4);
+
+  sails.config.flagpole.compoundDoc = true;
+
+  sails.request({
+    url   : '/author',
+    method: 'GET',
+  }, function (err, res, body) {
+    if (err) {
+      t.fail(err);
+    }
+    try {
+      t.ok(body.data[0].relationships, 'Body contains a "relationships" property');
+      t.ok(body.data[0].relationships.books, 'Relationships contains a "books" property');
+      t.ok(body.data[0].relationships.books.data, 'Relationships contains a "books" property');
+      t.equal(body.data[0].relationships.books.data[0].type, 'books', '"type" of first book is "books"');
+    } catch (err) {
+      t.fail(err);
+    }
+    t.end();
+  });
+});
+
+test('Returns included data', function (t) {
+  t.plan(8);
+
+  sails.config.flagpole.compoundDoc = true;
+  sails.config.flagpole.included = true;
+
+  sails.request({
+    url   : '/author',
+    method: 'GET',
+  }, function (err, res, body) {
+    if (err) {
+      t.fail(err);
+    }
+    try {
+      t.ok(body.included, 'Body contains an "included" property');
+      t.equal(body.included.length, 3, 'Three books are included');
+      t.ok(body.included[0].type, '"included" contains a "type" property');
+      t.equal(body.included[0].type, 'books', '"type" of included is "books"');
+      t.ok(body.included[0].id, 'Included data includes an "id" property');
+      t.ok(body.included[0].attributes, 'Included data includes an "attributes" property');
+      t.equal(typeof body.included[0].attributes, 'object', '"attributes" is an object');
+      t.equal(body.included[0].attributes.title, 'A Game of Thrones', 'Title of first book is "A Game of Thrones"');
+    } catch (err) {
+      t.fail(err);
+    }
+    t.end();
+  });
+});
+
+test('Does not return included data if "included = false"', function (t) {
+  t.plan(2);
+
+  sails.config.flagpole.compoundDoc = true;
+  sails.config.flagpole.included = false;
+
+  sails.request({
+    url   : '/author',
+    method: 'GET',
+  }, function (err, res, body) {
+    if (err) {
+      t.fail(err);
+    }
+    try {
+      t.notOk(body.included, 'Body does not contain an "included" property');
+      t.ok(body.data[0].relationships, 'But it still has a "relationships" property (compound doc)');
+    } catch (err) {
+      t.fail(err);
+    }
+    t.end();
+  });
+});
+
+test('Teardown', function (t) {
+  sails.lower(function () {
+    t.end();
+    process.exit(0); // A hack because otherwise tests won't end.  See https://github.com/balderdashy/sails/issues/2309
+  });
+});
